1e074fc00a07ec3108f3f7f9a5d8b6d0809c45ee
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2017-03-29 20:06:43 +0200

    Some changes

--- controllers/businessOperatorController.js
+++ controllers/businessOperatorController.js
@@ -1,15 +1,51 @@
 var mongoose = require('mongoose'),
 var Reservation = mongoose.model('Reservation');
+var Activity = mongoose.model('Activity');
 
 module.exports.viewReservations = 
 function(req,res) {
-var bussinessOperator = req.user    
-
-
-
-
+    var bussinessOperator = req.user    
+    if (bussinessOperator.type == "BuisnessOperator") {
+        var business = bussinessOperator.businessId
+        Activity.find({activitySchema},viewReservationsHelper)
+    }
 }
 
 
+function viewReservationsHelper(error,activities){
+    if(error){
+        res.send(JSON.stringify(error));
+    }else{
+        var activitiesId = returnIdsOnly(activities)
+        Reservation.find(function(error, reservations){
+            var resertionsBelongToOperator = Array()
+            for (i = 0; i < reservations.length; i++) { 
+                var reservationsActivityId = reservations[i].activityId
+                if(containsObject(reservationsActivityId,activitiesId)){
+                    resertionsBelongToOperator.push(reservations[i])
+                }
+            }
+            res.send(JSON.stringify(activitiesId)); 
+        })
+    }
+}
+
+function returnIdsOnly(modelArray){
+    var ids = Array()
+    for (i = 0; i < modelArray.length; i++) { 
+        ids.push(modelArray[i]._id)
+    }
+    return ids
+}
+
+function containsObject(obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
--- routes/main_routes.js
+++ routes/main_routes.js
@@ -1,9 +1,11 @@
 // routes file
 var express = require('express');
 var router = express.Router();
-var userController = require('../controllers/user_controller');
+//var userController = require('../controllers/user_controller');
+var userController = require('./businessOperatorRoutes');
 
-router.get('/', userController.index);
-router.post('/add', userController.create);
+
+// router.get('/', userController.index);
+// router.post('/add', userController.create);
 
 module.exports = router;
