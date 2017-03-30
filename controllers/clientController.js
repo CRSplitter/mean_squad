04099e26cad29ab76168e69a6df70a65836dc544
Mira Ekladious <miraekladious@gmail.com>
2017-03-30 15:33:46 +0200

    client cancel and view reservations

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -3,17 +3,20 @@ var Client = mongoose.model('Client');
 var Reservation = mongoose.model('Reservation');
 var Activity = mongoose.model('Activity');
 
+
 /*
-    Creates a new Reservation instance
+    Creates a new Reservation instance and returns success/failure message
     @params details, countParticipants, time, expirationInHours, clientId, activityId
+    @return json
     @mira
 */
+
 module.exports.makeReservation =[
     
     function(req,res,next){
 
         Activity.findById(activityId, function(err, Activity){
-            if(err) return err;
+            if(err) return res.json({error:"Error"});
             req.body.activity = Activity;
             next();
         });
@@ -45,8 +48,46 @@ module.exports.makeReservation =[
         Reservation.create(newReservation, function(err, Reservation){
             if(err) console.log(err);
             console.log(Reservation);
-            req.flash('success_msg', 'Reservation has been made successfully');
+            return res.json({message: 'Reservation has been made successfully'});
         });
 
     }
-]
+];
+
+
+/*
+    @param clientId
+    @return array of reservations
+    @mira
+ */
+
+module.exports.viewReservations = [
+
+    function(req,res,next){
+        var clientId = req.body.clientId;
+        Reservation.find({clientId:clientId}, function(err, results){
+            if(err) return res.json({error: "Error"});
+            return res.json({message:"Success", results});
+        });
+    }
+
+];
+
+
+/*
+    Removes a certain reservation
+    @param reservationId
+    @mira
+*/
+
+module.exports.cancelReservation = [
+
+    function(req,res,next){
+        var reservationId = req.body.reservationId;
+        Reservation.remove({_id: reservationId}, function(err, res){
+            if(err) return res.json({error: err});
+            res.json({message: "Reservation has been cancelled successfully"});
+        });
+    }
+
+];
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -3,4 +3,10 @@ var router = express.Router();
 var clientController = require('../controllers/clientController');
 
 // Posting a reservation
-router.post('/reserve', userController.makeReservation);
+router.post('/reserve', clientController.makeReservation);
+
+// Getting current Reservations
+router.get('/reservations', clientController.viewReservations);
+
+// Cancelling a reservation
+router.post('/reservations', clientController.cancelReservation);
