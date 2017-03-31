c0b38db5caaf566bb6f88e707e9f9b8f84c264e6
mekladious <mira.ekladious@gmail.com>
2017-04-01 01:00:44 +0200

    passport authentication for clients

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var clientController = require('../controllers/clientController');
 
 // Posting a reservation
-router.post('/reserve', clientController.makeReservation);
+router.post('/reserve', passport.authenticate('login'), clientController.makeReservation);
 
 // Getting current Reservations
-router.get('/reservations', clientController.viewReservations);
+router.get('/reservations', passport.authenticate('login'), clientController.viewReservations);
 
 // Cancelling a reservation
-router.post('/cancelReservations', clientController.cancelReservation);
+router.post('/cancelReservations', passport.authenticate('login'), clientController.cancelReservation);
 
 module.exports = router;
