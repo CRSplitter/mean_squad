b832868b59a331d84bc92c76ac8a39ce3d583dce
ielgohary <Islam.o.elgohary@gmail.com>
2017-04-01 17:08:10 +0200

    solved issue with activities tests

--- test/activityTests.js
+++ test/activityTests.js
@@ -17,28 +17,29 @@ describe('/GET view all activities', function () {
         Activity.collection.drop(() => {
             Activity.ensureIndexes(done);
         });
+    });
 
-        it("should return list of all activities in the database", function (done) {
-            const activity1 = new Activity({
-                name: "SE Project",
-                description: "Abandon all courses for one course"
-            });
-            activity1.save();
-
-            const activity2 = new Activity({
-                name: "The Office Marathon",
-                description: "fun that turns into an obligation"
-            });
-
-            activity2.save();
-
-            const activity3 = new Activity({
-                name: "read the big short",
-                description: "Investors betting on a global financial drama."
-            });
-
-            activity3.save();
+    it("should return list of all activities in the database", function (done) {
+        const activity1 = new Activity({
+            name: "SE Project",
+            description: "Abandon all courses for one course"
         });
+        activity1.save();
+
+        const activity2 = new Activity({
+            name: "The Office Marathon",
+            description: "fun that turns into an obligation"
+        });
+
+        activity2.save();
+
+        const activity3 = new Activity({
+            name: "read the big short",
+            description: "Investors betting on a global financial drama."
+        });
+
+        activity3.save();
+
         chai.request(server)
             .get('/activities')
             .end((err, res) => {
@@ -50,4 +51,5 @@ describe('/GET view all activities', function () {
                 done();
             });
     });
-})
+
+});
