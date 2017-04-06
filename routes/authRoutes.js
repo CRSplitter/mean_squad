b342db8946bbbeade0b796d22559de6c47959649
ameniawy <ameniawy18@gmail.com>
2017-04-06 15:49:06 +0200

    succes and failure fb routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
+var strings = require('../controllers/helpers/strings');
 
 
 router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
 router.get('/auth/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/login/profile',
-  failureRedirect: '/login/bla',
+  successRedirect: '/login/success',
+  failureRedirect: '/login/failed',
   failureFlash: 'true'
 }));
 
-router.get('/profile', function(req, res) {
-  res.send("logged in successfully");
+router.get('/success', function(req, res) {
+    res.json({
+        msg: 'Logged in with facebook successfully'
+    });
 });
 
-router.get('/', function(req, res, next) {
-  res.send("this is the homepage");
+router.get('/failed', function(req, res, next) {
+      return res.json({
+              errors: [{
+                  type:'Auth Error',
+                  msg: 'Logging in with facebook failed'
+              }]
+          });
 });
 
 
