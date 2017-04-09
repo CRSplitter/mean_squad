ee8225ad5e9de72c1a096e31f21637544d3dce93
ameniawy <ameniawy18@gmail.com>
2017-04-09 21:52:57 +0200

    filled in some of the docs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,15 +16,20 @@ var authMiddleware = require('../middlewares/authMiddleware');
 * @name /user/login POST
 * @example The route expects a body Object in the following format
 * {
-*     TODO
+* 		username: username of the user wishing to login,
+*		password: password of that username
+*
 * }
 * @example The route returns as a response an object in the following format
 * {
-*     msg: String showing a descriptive text,
-*     errors: TODO
+*     	msg: String showing a descriptive text,
+*     	errors: [Error],
+*		data: {user: user object}
+*	
 * }
 */
 router.post('/login', passport.authenticate("login"), userController.login);
+
 
 // POST foget password
 /**
@@ -33,7 +38,7 @@ router.post('/login', passport.authenticate("login"), userController.login);
 * @name /user/reset_password POST
 * @example The route expects a body Object in the following format
 * {
-*     TODO
+*     	email: email of the user wishing to reset his/her password
 * }
 * @example The route returns as a response an object in the following format
 * {
@@ -43,6 +48,7 @@ router.post('/login', passport.authenticate("login"), userController.login);
 */
 router.post('/reset_password', userController.forgetPassword);
 
+
 // GET reset password
 /**
 * A GET route responsible for TODO
@@ -50,7 +56,7 @@ router.post('/reset_password', userController.forgetPassword);
 * @name /user/reset/{toekn} GET
 * @example The route expects a body Object in the following format
 * {
-*     TODO
+*     	token: reset token in the params
 * }
 * @example The route returns as a response an object in the following format
 * {
@@ -60,6 +66,7 @@ router.post('/reset_password', userController.forgetPassword);
 */
 router.get('/reset/:token', userController.getResetPassword);
 
+
 // POST reset password
 /**
 * A POST route responsible for TODO
@@ -67,7 +74,8 @@ router.get('/reset/:token', userController.getResetPassword);
 * @name /user/reset/{token} POST
 * @example The route expects a body Object in the following format
 * {
-*     TODO
+*       token: reset token in the params,
+*       password: new password in the body
 * }
 * @example The route returns as a response an object in the following format
 * {
@@ -84,12 +92,11 @@ router.post('/reset/:token', userController.postResetPassword);
 * @name /user/logout GET
 * @example The route expects a body Object in the following format
 * {
-*     TODO
+*       
 * }
 * @example The route returns as a response an object in the following format
 * {
-*     msg: String showing a descriptive text,
-*     errors: TODO
+*     msg: String showing a descriptive text
 * }
 */
 router.get('/logout', userController.logout);
