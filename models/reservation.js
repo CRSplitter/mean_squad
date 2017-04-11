a9aeefa86b97d922b1b4367bedf2afa1951e24e4
mekladious <miraekladious@gmail.com>
2017-04-11 09:12:47 +0200

    Removed indexing from reservation model

--- models/reservation.js
+++ models/reservation.js
@@ -24,19 +24,19 @@ var reservationSchema = new Schema({
     }
 });
 
-reservationSchema.index({
-    "totalPrice": 1,
-    "details": 1,
-    "countParticipants": 1,
-    "confirmed": 1,
-    "time": 1,
-    "expirationInHours": 1,
-    "clientId": 1,
-    "activityId": 1
-}, {
-    unique: true,
-    validate: "You have already made this reservation"
-});
+// reservationSchema.index({
+//     "totalPrice": 1,
+//     "details": 1,
+//     "countParticipants": 1,
+//     "confirmed": 1,
+//     "time": 1,
+//     "expirationInHours": 1,
+//     "clientId": 1,
+//     "activityId": 1
+// }, {
+//     unique: true,
+//     validate: "You have already made this reservation"
+// });
 
 var Reservation = mongoose.model('Reservation', reservationSchema);
 module.exports = Reservation;
