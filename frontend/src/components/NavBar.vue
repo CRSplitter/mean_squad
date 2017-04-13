0236de8c91e493ab289a1ef4a47deee92d8ff5c3
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2017-04-13 14:20:59 +0200

    Added frontend structure

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>NavBar Here: Its Working </h1>
+    <h2>Try Route  http://localhost:8080/home </h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data () {
+    return {
+      msg: 'NavBar'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
