648088a0096e4677d16964fc52310544d62c0c22
Muhammad Khattab <msskmohammed96@gmail.com>
2017-04-15 15:03:47 +0200

    added more fields for business register form

--- backend/routes/businessRoutes.js
+++ backend/routes/businessRoutes.js
@@ -39,17 +39,16 @@ const upload = multer({
  * @name /business/register POST
  * @example The route expects a body Object in the following format
  * {
- *     username: username,
- *     password: password,
- *     confirmPassword: password,
- *     email: email of business,
- *     name: name of business,
- *     description: description,
- *     address: address,
- *     latitude: location latitude,
- *     longitude: location longitude,
- *     contactInfo: contactInfo
-
+ *     username: username(String),
+ *     password: password(String),
+ *     confirmPassword: password(String),
+ *     email: email of business(String),
+ *     name: name of business(String),
+ *     description: description(String),
+ *     address: address(String),
+ *     latitude: location latitude(Number),
+ *     longitude: location longitude(Number),
+ *     contactInfo: contactInfo(String)
  * }
  * @example The route returns as a response an object in the following format
  * {
--- frontend/src/components/registerForm.vue
+++ frontend/src/components/registerForm.vue
@@ -15,7 +15,25 @@
       <input type="email" v-model="email" name="email" class="form-control" id="inputEmail" placeholder="email" required>
 
       <label for="inputDate" v-if="formType === 'Client'" class="sr-only">Date of birth</label>
-      <input type="date" v-if="formType === 'Client'" v-model="dateOfBirth" name="dateOfBirth" class="form-control" id="inputDate" placeholder="Date of birth" required>
+      <input type="date" v-if="formType === 'Client'" v-model="dateOfBirth" name="dateOfBirth" class="form-control" id="inputDate" placeholder="date of birth" required>
+
+      <label for="inputName" v-if="formType === 'Business'" class="sr-only">Title</label>
+      <input type="text" v-if="formType === 'Business'" v-model="name" name="name" class="form-control" id="inputName" placeholder="business name or title" required>
+
+      <label for="inputDescription" v-if="formType === 'Business'" class="sr-only">Description</label>
+      <input type="text" v-if="formType === 'Business'" v-model="description" name="description" class="form-control" id="inputDescription" placeholder="description" required>
+
+      <label for="inputAddress" v-if="formType === 'Business'" class="sr-only">Address</label>
+      <input type="text" v-if="formType === 'Business'" v-model="address" name="address" class="form-control" id="inputAddress" placeholder="address" required>
+
+      <label for="inputContact" v-if="formType === 'Business'" class="sr-only">Contact info</label>
+      <input type="text" v-if="formType === 'Business'" v-model="contactInfo" name="contactInfo" class="form-control" id="inputContact" placeholder="contact info" required>
+
+      <label for="inputLatitude" v-if="formType === 'Business'" class="sr-only">Latitude</label>
+      <input type="number" v-if="formType === 'Business'" v-model="latitude" name="latitude" class="form-control" id="inputLatitude" placeholder="latitude" required>
+
+      <label for="inputLongitude" v-if="formType === 'Business'" class="sr-only">Longitude</label>
+      <input type="number" v-if="formType === 'Business'" v-model="longitude" name="longitude" class="form-control" id="inputLongitude" placeholder="longitude" required>
 
       <input type="submit" class="btn btn-lg btn-danger" value="Sign up">
     </form>
@@ -39,7 +57,13 @@
                 password: '',
                 confirmPassword: '',
                 email: '',
-                dateOfBirth: null,
+                dateOfBirth: '2000-12-31',
+                name: '',
+                longitude: 0,
+                latitude: 0,
+                contactInfo: '',
+                description: '',
+                address: '',
                 errors: []
             }
         },
@@ -68,6 +92,12 @@
                         });
                 } else {
                     if (this.formType === 'Business') {
+                        user.name = this.name;
+                        user.address = this.address;
+                        user.description = this.description;
+                        user.longitude = this.longitude;
+                        user.latitude = this.latitude;
+
                         this.$http.post('http://localhost:8080/business/register', user)
                             .then(function(res) {
                                 console.log(res);
