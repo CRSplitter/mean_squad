1e193bf4dd76627df901b3e759f05dd226bbfd8e
Muhammad Khattab <msskmohammed96@gmail.com>
2017-04-16 13:12:16 +0200

    added delete activity button functionality
     - fixed docs

--- frontend/src/components/activityCard.vue
+++ frontend/src/components/activityCard.vue
@@ -8,13 +8,22 @@
         <p class="card-text">{{ activity.description }}</p>
 
         <div v-if="user" class="row">
-            <button v-if="user.type === 'Client' || user.type === 'Business'" v-on:click="reserve" class="btn btn-success offset-md-1">reserve</button>
-            <button v-if="user.type === 'Business'" v-on:click="edit" class="btn btn-primary offset-md-1">edit</button>
+            <button v-if="user.type === 'Client' || user.type === 'Business'" v-on:click="reserve" class="btn btn-success offset-md-1">Reserve</button>
+            <button v-if="user.type === 'Business'" v-on:click="edit" class="btn btn-primary offset-md-1">Edit</button>
             <form v-if="user.type === 'Business'" v-on:submit="del">
                 <input type="submit" class="btn btn-danger offset-mid-1" value="Delete">
             </form>
         </div>
       </div>
+    </div>
+
+    <div v-if="errors.length > 0">
+        <div class="alert alert-danger" role="alert">
+            <strong>Oh snap!</strong>
+            <div v-for="error in errors">
+                {{ error.msg }}
+            </div>
+        </div>
     </div>
 
 </div>
@@ -28,13 +37,26 @@
         name: 'ActivityCard',
         data() {
             return {
-                user: user
+                user: user,
+                errors: [],
             }
         },
         methods: {
             reserve: function() {},
             edit: function() {},
-            del: function() {}
+            del: function() {
+                this.$http.post('http://localhost:8080/business/removeActivity', activity._id)
+                    .then(function(res) {
+                        console.log(res);
+                        if (res.body.errors) {
+                            this.errors = res.body.errors;
+                        } else {
+                            // TODO success
+                        }
+                    }, function(res) {
+                        console.log("error");
+                    });
+            }
         }
     }
 </script>
