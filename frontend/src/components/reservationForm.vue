0f3ef1af4eb9f24ca3cfbdb5ebead9c6ba14d863
ameniawy <ameniawy18@gmail.com>
2017-04-16 21:11:55 +0200

    done with reservation form

--- frontend/src/components/reservationForm.vue
+++ frontend/src/components/reservationForm.vue
@@ -1,4 +1,5 @@
 <template>
+
     <div class="container text-center">
         <div v-if="errors.length > 0">
             <div class="alert alert-danger">
@@ -8,9 +9,10 @@
                 </div>
             </div>
         </div>
-        <div v-if="msg.length != 0">
+        <div v-if="msg.length != 0" class="alert alert-success">
             <strong>{{msg}}</strong>
         </div>
+
 
         <form v-on:submit="reserve">
             <!--<label for="details" class="sr-only">Details</label>-->
@@ -18,37 +20,47 @@
             <br>
 
             <label for="numberOfParticipants" class="sr-only">Number Of Participants</label>
-            <input type="number" min="1" v-model="countParticipants" name="countParticipants" id="numberOfParticipants" class="form-control" placeholder="participants"
-                required>
+            <input type="number" min="1" v-model="countParticipants" name="countParticipants" id="numberOfParticipants" class="form-control"
+                placeholder="participants" required>
 
-        <div v-if="activity">
-            <div v-for="day in activity.activitySlots">
-                <div v-if="day.slots.length != 0">
-                    <h2>{{ day.day }}</h2>
-
-                    <select class="form-control" id="sel1" v-model="object">
-                        <option v-for="slot in day.slots" :value="{ slot: slot, day: day }">
-                            {{day.day}} {{slot.time}}
+            <br>
+            <div v-if="activity">
+                <select class="form-control" v-model="selected">
+                    <optgroup v-for="day in activity.activitySlots" v-if="day.slots.length != 0" :label="day.day">
+                        <option v-for="slot in day.slots" :value="{slot: slot, day: day}">
+                            {{day.day}} {{slot.time}} <strong>Slots Remaining: </strong> {{slot.maxParticipants - slot.currentParticipants}}
                         </option>
-                    </select>
-
-
-                    <!--<li v-for="slot in day.slots">
-                        <input v-model="picked" :value="day.day + ' ' + slot.time" name="chosen" v-if="slot.currentParticipants != slot.maxParticipants" type="radio" 
-                        v-on:click="choose(slot, day, activity._id)">
-                        <strong>{{day.day}} {{slot.time}}</strong>    </input>
-
-                        Available Slots Remaining: <strong>{{ slot.maxParticipants -  slot.currentParticipants}}</strong>
-                    </li>-->
-                    
-                </div>
+                    </optgroup>
+                </select>
             </div>
-        </div>
-        <br><br>
-            <h3>{{activity.name}}</h3><hr>
-            <p>Date Picked: <strong>{{object.slot}}</strong></p>
-            <p v-if="countParticipants.length != 0">Participants: <strong>{{countParticipants}}</strong></p>
+
+
+            <br><br>
+            <h3 v-if="activity">{{activity.name}}</h3>
+            <hr>
+            <table class="table">                
+            <tr v-if="selected.day.length != 0">
+                <td>Date</td>
+                <td>{{selected.day.day}} {{selected.slot.time}}</td>
+            </tr>
+            <tr v-if="countParticipants != 0">
+                <td>Participants</td>
+                <td>{{countParticipants}}</td>
+            </tr>
+            <tr v-if="details.length != 0">
+                <td>Details</td>
+                <td>{{details}}</td>
+            </tr>
+            <tr v-if="totalPrice != 0">
+                <td>Total Price</td>
+                <td>{{totalPrice}} LE</td>
+
+            </tr>
+            </table>
+            <!--<p v-if="selected.day.length != 0">Date: <strong>{{selected.day.day}} {{selected.slot.time}}</strong></p>
+            <p v-if="countParticipants != 0">Participants: <strong>{{countParticipants}}</strong></p>
             <p v-if="details.length != 0">Details: <strong>{{details}}</strong></p>
+            <p v-if="totalPrice != 0">Total Price: <strong>{{totalPrice}} LE</strong></p>-->
             <br>
             <input type="submit" class="btn btn-lg btn-danger" value="Reserve">
         </form>
@@ -64,48 +76,60 @@
         name: 'register',
         data() {
             return {
-                countParticipants: '',
+                countParticipants: 0,
                 details: '',
-                activityId: '',
-                dayId: '',
-                slotId: '',
                 errors: [],
-                picked: '',
                 msg: '',
-                object: ''
+                selected: {
+                    day: '',
+                    slot: ''
+                }                
             }
         },
         methods: {
-            reserve: function(e) {
+            reserve: function (e) {
                 e.preventDefault();
+                console.log(this.time);
                 var reservation = {
-                    dayId: this.dayId,
-                    slotId: this.object.slot._id,
+                    dayId: this.selected.day._id,
+                    slotId: this.selected.slot._id,
                     countParticipants: this.countParticipants,
                     details: this.details,
-                    activityId: this.activityId
+                    activityId: this.activity._id
                 };
                 console.log(reservation);
                 var uri = 'http://localhost:8080/client/makereservation';
 
                 this.$http.post(uri, reservation)
-                    .then(function(res){
-                        if(res.data.errors) {
+                    .then(function (res) {
+                        if (res.data.errors) {
                             this.errors = res.data.errors;
-                        }
-                        else {
+                        } else {
                             this.msg = res.data.msg;
                             console.log(res.data);
                         }
                     });
-            },
-            choose: function(slot, day, aId, e) {
-                console.log("henaa");
-                this.slotId = slot._id;
-                this.dayId = day._id;
-                this.activityId = aId;
-                //this.picked = day.day + ' at ' + slot.time;
+            }
+        },
+        computed: {
+            totalPrice: function() {
+                if(this.activity) {
+                    return this.countParticipants * this.activity.price;
+                }
+                return 0;
             }
         }
     }
 </script>
+<style>
+table {
+    border-collapse: collapse;
+    width: 50%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+</style>
