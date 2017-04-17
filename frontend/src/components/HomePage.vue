a6cd2152229ee9d6f2dd74f33ee46b7494de8b9f
ameniawy <ameniawy18@gmail.com>
2017-04-17 12:05:48 +0200

    bug fix

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,30 +1,32 @@
 <template>
   <div>
-    <div>
-        <businessCard :business = business></businessCard>   
-         <!--v-bind: business(defined in props of child component) = business(data model property)-->
-      </div>
+    <h1>Hello</h1>
+    <BusinessCard v-bind:business=business></BusinessCard>
+    <!--v-bind: business(defined in props of child component) = business(data model property)-->
   </div>
 </template>
 
 <script>
-  import businessCard from './businessCard';
+  import BusinessCard from './businessCard';
   export default {
-    props:[],
+    props: [],
     name: 'HomePage',
-    data () {
+    data() {
       return {
         msg: 'HomePage',
+        activity: {
+          name: "MENIAWY EL GAMED"
+        },
         business: {
           name: 'Scuba',
           description: 'gamed',
           avgRating: 2,
           contactInfo: ['scuba@duba', '23443']
-        }, 
-        components: {
-          businessCard: businessCard
         }
       }
+    },
+    components: {
+      BusinessCard: BusinessCard
     }
   }
 </script>
