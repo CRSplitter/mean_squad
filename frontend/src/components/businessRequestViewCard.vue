c98b3d73cfbe41b5acfd0d54560218713f556d2a
mekladious <miraekladious@gmail.com>
2017-04-18 01:35:44 +0200

    business request view card

--- frontend/src/components/businessRequestViewCard.vue
+++ frontend/src/components/businessRequestViewCard.vue
@@ -3,14 +3,14 @@
 
     <div class="card card-outline-danger text-center">
       <div class="card-block">
-        <h3 class="card-title">{{ Business.name }}</h3>
-        <h4 class="card-title">{{ Business.description }}</h4>
-        <h4 class="card-title">{{ Business.address }}</h4>
-        <h4 class="card-title">{{ Business.contactInfo }}</h4>
-        <h4 class="card-title">{{ Business.approved }}</h4>
+        <h3 class="card-title" v-if="business.name">{{ business.name }}</h3>
+        <!--<h4 class="card-title">{{ business.description }}</h4>
+        <h4 class="card-title">{{ business.address }}</h4>
+        <h4 class="card-title">{{ business.contactInfo }}</h4>
+        <h4 class="card-title">{{ business.approved }}</h4>-->
         <div class="row">
             <button v-on:click="accept" class="btn btn-success offset-md-1">Accept</button>
-            <button v-on:click="reject" class="btn btn-success offset-md-1">Reject</button>
+            <button v-on:click="reject" class="btn btn-failure offset-md-1">Reject</button>
         </div>
       </div>
     </div>
@@ -20,47 +20,48 @@
 </template>
 
 <script>
-var URL = require('../env.js').HostURL;
-export default {
-    props: ['Business'],
-    name: 'businessRequestViewCard',
-    data() {
-        return {}
-    },
-    methods: {
-        accept: function(e) {
-            e.preventDefault();
-            this.$http.post(URL+'/business/'+this.Business._id+'/accept', {
-                    businessId: this.Business._id
-                })
-                .then(function(res) {
-                    console.log(res);
-                    if (res.body.errors) {
-                        this.errors = res.body.errors;
-                    } else {
-                        // TODO success
-                    }
-                }, function(res) {
-                    console.log("error");
-                });
+    var URL = require('./env.js').HostURL;
+    export default {
+        props: ['business'],
+        name: 'businessRequestViewCard',
+        data() {
+            return {}
         },
+        methods: {
+            accept: function(e) {
+                console.log(this.business.name);
+                e.preventDefault();
+                this.$http.post(URL + '/business/' + this.business._id + '/accept', {
+                        businessId: this.business._id
+                    })
+                    .then(function(res) {
+                        console.log(res);
+                        if (res.body.errors) {
+                            this.errors = res.body.errors;
+                        } else {
+                            // TODO success
+                        }
+                    }, function(res) {
+                        console.log("error");
+                    });
+            },
 
-        reject: function(e){
-            e.preventDefault();
-            this.$http.post(URL+'/business/'+this.Business._id+'/reject', {
-                    businessId: this.Business._id
-                })
-                .then(function(res) {
-                    console.log(res);
-                    if (res.body.errors) {
-                        this.errors = res.body.errors;
-                    } else {
-                        // TODO success
-                    }
-                }, function(res) {
-                    console.log("error");
-                });
+            reject: function(e) {
+                e.preventDefault();
+                this.$http.post(URL + '/business/' + this.business._id + '/reject', {
+                        businessId: this.business._id
+                    })
+                    .then(function(res) {
+                        console.log(res);
+                        if (res.body.errors) {
+                            this.errors = res.body.errors;
+                        } else {
+                            // TODO success
+                        }
+                    }, function(res) {
+                        console.log("error");
+                    });
+            }
         }
     }
-}
 </script>
