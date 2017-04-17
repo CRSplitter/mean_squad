1086d8c4bff521990aad4ee48df0d34e120579b5
carsoli <caroleemad96@gmail.com>
2017-04-17 12:13:48 +0200

    nvm

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -14,38 +14,16 @@
     name: 'HomePage',
     data () {
       return {
-        msg: 'HomePage',
-        business: {
-          name: 'Scuba',
-          description: 'gamed',
-          avgRating: 2,
-          contactInfo: ['scuba@duba', '23443']
-        }, 
-        components: {
-          businessCard: businessCard
+          business: {
+            name: 'Scuba',
+            description: 'gamed',
+            avgRating: 2,
+            contactInfo: ['scuba@duba', '23443']
+          }
         }
+      },
+    components: {
+        businessCard: businessCard
       }
     }
-  }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<!--<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>-->
