ddf0124264e265e0ca6dd75649de98c5cfcda340
mekladious <miraekladious@gmail.com>
2017-04-17 19:28:29 +0200

    added admin approve business route

---
+++ frontend/src/components/businessRequestViewCard.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+
+    <div class="card card-outline-danger text-center">
+      <div class="card-block">
+        <h3 class="card-title">{{ businessObject.name }}</h3>
+        <h4 class="card-title">{{ businessObject.description }}</h4>
+        <h4 class="card-title">{{ businessObject.address }}</h4>
+        <h4 class="card-title">{{ businessObject.contactInfo }}</h4>
+        <h4 class="card-title">{{ businessObject.approved }}</h4>
+        <div class="row">
+            <button v-on:click="approve" class="btn btn-success offset-md-1" v-if="!businessObject.approved">Approve</button>
+        </div>
+      </div>
+    </div>
+
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['businessObject'],
+        name: 'businessRequestViewCard',
+        data() {
+            return {}
+        },
+        methods: {
+            approve: function() {}
+        }
+    }
+</script>
