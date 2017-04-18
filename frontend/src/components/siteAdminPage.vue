eaef588fd5ccfa8931fca30e3a729363c4147ebb
mekladious <miraekladious@gmail.com>
2017-04-18 19:58:18 +0200

    add admin and view requests

--- frontend/src/components/siteAdminPage.vue
+++ frontend/src/components/siteAdminPage.vue
@@ -1,52 +1,71 @@
 <template>
     <div>
-        <div class="container">
+        <div class="container" v-if='adminForm'>
             <div class="row">
-                <button v-on:click="viewRequests" class="btn btn-danger">Business requests</button>
-                <button v-on:click="addAdmin" class="btn btn-danger">Add admin</button>
-                <li v-for="business in pendingBusinesses" v-show="showRequests">
-                    <businessRequest :business="business"></businessRequest>
-                </li>
+                <button @click="viewRequests" class="btn btn-danger">Business requests</button>
+                &nbsp;
+                <button @click="addAdmin" class="btn btn-danger" data-toggle="modal" data-target="#registerModal">Add admin</button>
+                <br>
             </div>
+            <div class= "container">
+                <br>
+                <li style="list-style: none;" v-if="pendingBusinesses" v-for="business in pendingBusinesses" v-show="showRequests">
+               <businessRequestViewCard :business="business"></businessRequestViewCard>
+               <br>
+                </li>
+              </div>
+              <div v-if='adminForm === true' v-show="showForm">
+                <registerPage :adminForm="adminForm"></registerPage>
+              </div>
+             
         </div>
     </div>
 </template>
 
 <script>
-    import registerForm from './registerForm';
+    import registerPage from './registerPage';
     import businessRequestViewCard from './businessRequestViewCard';
 
     var URL = require('./env.js').HostURL;
 
     export default {
+        props: [],
         name: 'siteAdminPage',
         data() {
             return {
                 pendingBusinesses: [],
+                adminForm: true,
                 showRequests: false,
                 showForm: false
             }
         },
         methods: {
-            viewRequests: function(){
-                    this.$http.get('http://localhost:8080/admin/viewBusinessRequests')
-                .then(function(res){
-                    pendingBusinesses= res.data.data.businesses;
-                    showRequests = true;
-                    showForm = false;
-                })
-            },
-            addAdmin: function(){
-               this.$http.get('http://localhost:8080/admin/register')
-                .then(function(res){
-                    
-                    showRequests = false;
-                    showForm = true; 
-                })
-            }
+
+            viewRequests: function(e) {
+                    e.preventDefault();
+                    this.$http.get(URL+'/admin/viewBusinessRequests')
+                        .then(function(res) {
+                            console.log(res);
+                            this.pendingBusinesses = res.data.data.businesses;
+                            this.showRequests = true;
+                            this.showForm = false;
+                            
+                        }, function(res) {
+                            console.log("error");
+                        });
+                },
+                 addAdmin: function() {
+                     this.showRequests = false;
+                     this.showForm = true;
+                     console.log(this.adminForm);
+                 }
         },
         components: {
-            businessRequest: businessRequestViewCard
+            businessRequestViewCard: businessRequestViewCard,
+            registerPage: registerPage
+        },
+        created: function(){
+            this.adminForm=true;
         }
     }
 </script>
