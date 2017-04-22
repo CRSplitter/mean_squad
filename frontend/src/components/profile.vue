a56b2f927da53a2e9e1fe9de723457e2b99f5855
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2017-04-22 17:20:20 +0200

    profile Image size

--- frontend/src/components/profile.vue
+++ frontend/src/components/profile.vue
@@ -197,5 +197,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  img{
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+  }
 </style>
