bb391a3b087d3914cade301890e0919b1fe40f86
ameniawy <ameniawy18@gmail.com>
2017-04-27 22:13:55 +0200

    added some labels

--- frontend/src/components/reservationDetailedView.vue
+++ frontend/src/components/reservationDetailedView.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="reservation" class="container box_shadow" style=" border-radius: 10px; padding: 20px; width: 450px; text-align: center;             background-color: white;
 ">
-        <h3>#{{reservation._id}}</h3>
+        <h3>{{frontFormat(this.reservation.date)}}</h3>
         {{disableCancel()}}
         <div style="text-align: center">
         <br>
@@ -12,7 +12,7 @@
         <br>
         <div style="text-align: left; margin-left: auto; margin-right: auto; width: 200px;">
             <div v-if="userType=='Client'">
-            <span class="label">Reserved By:</span><span v-if="reservation">{{reservation.clientId.userId.name}}</span><br>
+            <!--<span class="label">Reserved By:</span><span v-if="reservation">{{reservation.clientId.userId.name}}</span><br>-->
             </div>
             <span class="label">Number of participants: </span><span v-if="reservation">{{reservation.countParticipants}}</span><br>
             <span class="label">Details: </span><span v-if="reservation">{{reservation.details}}</span><br>
